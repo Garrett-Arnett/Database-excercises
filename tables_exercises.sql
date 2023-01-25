392edd2c04ec0eb5022ad3e68f1ad4d5609ddd28
“Garrett <garrett.paul.arnett@gmail.com>
2023-01-25 15:50:32 -0600

    test commit with text change

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -11,3 +11,4 @@ DESCRIBE titles;
 -- Which table(s) do you think contain a date type column? I believe the tables that show date columns are dept_emp, dept_manager_employees, employees, salaries and titles;
 -- What is the relationship between the employees and the departments tables? The employees have an employee number. You can use this to track back to see which department they work in the dept_emp table. Once you find the dept number, you can then search it in the departments table.;
 DESCRIBE dept_manager;
+-- test
