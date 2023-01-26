ae53877f3f0e16834529dcd561db7595403dd1a6
“Garrett <garrett.paul.arnett@gmail.com>
2023-01-26 11:46:41 -0600

    addition 2

--- select_exercises.sql
+++ select_exercises.sql
@@ -16,9 +16,9 @@ SELECT * FROM albums WHERE Name LIKE 'Nevermind';
 -- The genrea for the album Nevermind is Grunge, Alternative rock.
 SELECT * FROM albums WHERE release_date between 1990 and 1999;
 -- There where 11 albumbs released in the 90s.
-SELECT * FROM albums WHERE sales < 20.0;
+SELECT name FROM albums WHERE sales < 20.0;
 -- THere are 13 albumbs that have less than 20 million sales.
-SELECT name FROM albums WHERE genre LIKE 'rock';
+SELECT * FROM albums WHERE genre LIKE 'rock';
 SELECT * FROM albums WHERE genre LIKE '%rock';
 -- When you search for the genre 'rock'. It will pull just the rock genre. You have to add % before rock (%rock) to search genres in the areas of rock, hard rock, and progressive rock.
 
