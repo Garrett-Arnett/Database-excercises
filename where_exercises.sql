12cd343bbde98c635bc5190d27971ddc5c343c4a
“Garrett <garrett.paul.arnett@gmail.com>
2023-01-26 13:51:37 -0600

    review 1.3

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,11 +13,13 @@ SELECT Count(*) FROM employees WHERE last_name LIKE '%E' or last_name LIKE 'E%';
 -- There are 30723 employees with a last name that starts or ends with e. THere are 23390 with employees name who do not start with e and but end with an e;
 SELECT Count(*) FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
 SELECT Count(*) FROM employees WHERE last_name LIKE '%E';
--- There are 899 employees who start and end with an e. There are 24292 employees whose last names end with the letter execute
+-- There are 899 employees who start and end with an e. There are 24292 employees whose last names end with the letter e. Can also use LIKE 'E%e'
 SELECT Count(*) FROM employees where hire_date BETWEEN '1990-01-01' AND '1999-12-31';
--- There are 135214 employees that where hired in the 90s.
+-- There are 135214 employees that where hired in the 90s. or LIKE '199%';
 Select Count(*) FROM employees where birth_date LIKE '%-12-25';
--- There are 842 employees who were born on Christmas
+-- There are 842 employees who were born on Christmas. or LIKE '%12-25'
+SELECT Count(*) FROM employees where hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%-12-25';
+-- 362 employees born in the 90s on christmas
 SELECT count(*) FROM employees WHERE last_name LIKE '%Q%';
 -- There are 1873 poeple with a q in thier last name
 SELECT count(*) FROM employees WHERE last_name LIKE '%Q%' AND last_name NOT LIKE '%QU%';
